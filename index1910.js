fb71a61e71d0572a5943b29601e19855838a8df0
sumitdevraj <preetam@dice-academy.com>
2024-10-19 17:41:30 +0530

    added find duplictes

--- index1910.js
+++ index1910.js
@@ -16,28 +16,46 @@ function bubbleSort(arr) {
             if (arr[j] > arr[j + 1]) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
             }
-            
+
         }
-        console.log("first itration",i, arr);
-        
+        console.log("first itration", i, arr);
+
     }
     return arr
 }
 // console.log(bubbleSort([8, 7, 6, 5, 4, 3, 2, 1]))
-let arr =[8, 7, 6, 5, 4, 3, 2, 1]
-function selectionSort(arr){
-    for(let i = 0; i < arr.length; i++) {
+let arr = [8, 7, 6, 5, 14, 3, 2, 1,8,1,7]
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
         let min_idx = i;
-      for(let j=i+1; j < arr.length; j++) {
-        if(arr[j]<arr[min_idx]){
-            min_idx = j;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
         }
-      }  
-      let temp=arr[i];
-      arr[i] =arr[min_idx];
-      arr[min_idx] = temp;
+        let temp = arr[i];
+        arr[i] = arr[min_idx];
+        arr[min_idx] = temp;
+
+        console.log(`log iteration ${i} arr ${arr}`);
+
+    }
+    return arr;
 }
- return arr;
+// console.log(selectionSort(arr));
+function findDuplicates(arr) {
+    let set = new Set();
+    let duplicate = [];
+    for (let i of arr) {
+        if (set.has(i)) {
+            duplicate.push(i);
+        }
+        else {
+            set.add(i);
+        }
+    }
+    return duplicate;
 }
-console.log(selectionSort(arr));
+console.log(findDuplicates(arr));
+
 
