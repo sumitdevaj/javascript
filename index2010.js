5d0e9e8a63cb28e573f10f8c6453d6966a7a114f
sumitdevraj <preetam@dice-academy.com>
2024-10-20 17:13:23 +0530

    pushed

--- index2010.js
+++ index2010.js
@@ -7,11 +7,30 @@ let newArr= arr.slice(0,-5);
 
 let arr1= arr.splice(2,2,["hello","Ffdd"])
 // console.log('return array',arr1 ,'original array',arr);
-console.log(arr1);
+// console.log(arr1);
  
-let arr2 = [1,2,3,4];
-    arr2.splice(1,2,'x','y')
-console.log(arr2);
+let arr2 = [1,3,2,10,4,3,10];
+    // arr2.splice(1,2,'x','y')
+// console.log(arr2);
+// console.log(arr2.includes(3,2))
  
+// console.log(arr2.indexOf(10));
+// for(let i of arr2){
+//     console.log(i); 
+// }
+let forEachArr = arr2.forEach((val,index)=>{    
+    // console.log(val*3)
+    
+})
+// console.log(forEachArr);
+// let sumOfArr= arr2.reduce((acc,val,index,arr)=>{
+//    return acc+=val
+// })
+// console.log(sumOfArr);
+
+// sorting 
+let sortingArr = [45,23,32,12,76,3,54]
+
+
 
 
