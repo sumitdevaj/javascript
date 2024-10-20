79c5a7c46234187be067a6bf304a832c1ccf728d
sumitdevraj <preetam@dice-academy.com>
2024-10-20 16:22:27 +0530

    pushed

---
+++ index2010.js
@@ -0,0 +1,17 @@
+// array methods
+let arr =[1,2,3,4,5,6,7,8];
+let newArr= arr.slice(0,-5);
+// console.log(newArr);
+
+// splice method 
+
+let arr1= arr.splice(2,2,["hello","Ffdd"])
+// console.log('return array',arr1 ,'original array',arr);
+console.log(arr1);
+ 
+let arr2 = [1,2,3,4];
+    arr2.splice(1,2,'x','y')
+console.log(arr2);
+ 
+
+
