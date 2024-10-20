f4a0b8cdc16d9d789f6106637a1deecc8e50c309
sumitdevraj <preetam@dice-academy.com>
2024-10-20 17:55:15 +0530

    push

--- index2010.js
+++ index2010.js
@@ -1,26 +1,26 @@
 // array methods
-let arr =[1,2,3,4,5,6,7,8];
-let newArr= arr.slice(0,-5);
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+let newArr = arr.slice(0, -5);
 // console.log(newArr);
 
 // splice method 
 
-let arr1= arr.splice(2,2,["hello","Ffdd"])
+let arr1 = arr.splice(2, 2, ["hello", "Ffdd"])
 // console.log('return array',arr1 ,'original array',arr);
 // console.log(arr1);
- 
-let arr2 = [1,3,2,10,4,3,10];
-    // arr2.splice(1,2,'x','y')
+
+let arr2 = [1, 3, 2, 10, 4, 3, 10];
+// arr2.splice(1,2,'x','y')
 // console.log(arr2);
 // console.log(arr2.includes(3,2))
- 
+
 // console.log(arr2.indexOf(10));
 // for(let i of arr2){
 //     console.log(i); 
 // }
-let forEachArr = arr2.forEach((val,index)=>{    
+let forEachArr = arr2.forEach((val, index) => {
     // console.log(val*3)
-    
+
 })
 // console.log(forEachArr);
 // let sumOfArr= arr2.reduce((acc,val,index,arr)=>{
@@ -35,12 +35,20 @@ let forEachArr = arr2.forEach((val,index)=>{
 
 // filter 
 
-let filterArr = [45,23,32,12,76,3,54];
-let evenVal= filterArr.filter((val)=>val%2 !==0)
-// console.log(evenVal);
-let dd= [94,4,2,34,40,22,23,12,4] 
-let res = dd.filter((val)=> val >30).map((val)=>val*2).reduce((acc,val)=>acc+=val)
-console.log(res);
+// let filterArr = [45, 23, 32, 12, 76, 3, 54];
+// let evenVal = filterArr.filter((val) => val % 2 !== 0)
+// // console.log(evenVal);
+// let dd = [94, 4, 2, 34, 40, 22, 23, 12, 4]
+// let res = dd.filter((val) => val > 30).map((val) => val * 2).reduce((acc, val) => acc += val)
+// console.log(res);
+
+
+// find method 
+let findArr = [12,34,32,53,43,2];
+console.log(findArr.findIndex((val)=>val === 43));
+
+let aa= findArr.concat([22,222,33,333])
+console.log(aa);
 
 
 
