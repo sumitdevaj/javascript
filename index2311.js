85c05dc2a8ab12f5f518c60f12ddbbd2c0bb79e0
sumitdevraj <preetam@dice-academy.com>
2024-11-24 17:23:59 +0530

    djnjkdkd

--- index2311.js
+++ index2311.js
@@ -45,24 +45,116 @@ let myMethod ={
         return this.age >= 18;
      },
      about : function(){
-        return this
+        return `Name :${this.name}  age :${this.age} email :${this.email}`;
     }
 
 }
 
-function CreateUser(name,age,email){
-    const user = {};
-    user.name = name;
-    user.age = age;
-    user.email = email;
-    user.is18 = myMethod.is18
-    user.about = myMethod.about
-    return user;
+// function CreateUser(name,age,email){
+//     const user = {}
+//     user.name = name;
+//     user.age = age;
+//     user.email = email;
+//     user.is18 = myMethod.is18
+//     user.about = myMethod.about
+//     return user;
+// }
+
+
+
+
+// function CreateUser(name,age,email){
+//     const user = Object.create(CreateUser.prototype);
+//     user.name = name;
+//     user.age = age;
+//     user.email = email;
+//     // user.is18 = myMethod.is18
+//     // user.about = myMethod.about
+//     return user;
+// }
+// let user1 = CreateUser("pritam",17,"prdcmdp@gmail.com")
+// // console.log(user1);
+// CreateUser.prototype.is18 =function(){
+//     return this.age >= 18;
+//  }
+//  CreateUser.prototype.about =function(){
+//     return `Name :${this.name}  age :${this.age} email :${this.email}`;
+// }
+// console.log(CreateUser.prototype);
+
+
+
+// let user2 = CreateUser("arbaz",27,"arbazmdp@gmail.com")
+// console.log(user1.__proto__);
+// console.log(user2);
+
+
+// creation of object using new keyword 
+
+// function CreateObject(name,email,password){
+//     this.name=name;
+//     this.email=email;
+//     this.password=password;
+//     this.is20 = function(){
+//         console.log("kfmff");
+        
+//     }
+// }
+
+// let objUser1 =new CreateObject("pritam","vjdnkldckd",12334);
+// console.log(objUser1);
+function joinNewUser(name,email,phone,address,age,role){
+    this.name=name;
+    this.email=email;
+    this.phone=phone;
+    this.address=address;
+    this.age = age;
+    this.role=role;
 }
-let user1 = CreateUser("pritam",17,"prdcmdp@gmail.com")
-let user2 = CreateUser("arbaz",27,"arbazmdp@gmail.com")
-console.log(user1.is18());
-console.log(user2);
+
+function CreateBank (bankName,ifsc,address,branch,helpline,atm){
+    this.bankName=bankName;
+    this.ifsc=ifsc;
+    this.address=address;
+    this.branch=branch;
+    this.listOfUsers=[];
+    this.helpline=helpline;
+    this.atm=atm;
+    this.listOfEmp=[];
+    this.balance=[];
+    this.join= function (name,email,phone,address,age,role){
+        switch(role){
+            case "EMPLOYEE":
+                this.listOfEmp.push(new joinNewUser(name,email,phone,address,age,role))
+                break
+            case "CUSTOMER":
+                this.listOfUsers.push(new joinNewUser(name,email,phone,address,age,role))
+                break
+            default:
+                throw new Error("bad role")
+    }
+}
+    this.chequeApply= function (){
+        
+    }
+    this.withdrawal= function (){}
+    this.deposit = function (name,amount){
+        for(let user of this.listOfUsers){
+            if(user.name==name){
+                this.balance.push({name,amount})
+            }
+        }
+    }
+
+}
+let AxisBank = new CreateBank('AxisBank',"kfkcmkf","tilak nagar","TN",'123443221',5)
+AxisBank.join('pritam',"vjndjrd@gmial.com","6543223456","bfjnrijdfv",12,"EMPLOYEE")
+AxisBank.join('sumit',"vjndjrd@gmial.com","6543223456","bfjnrijdfv",12,"CUSTOMER")
+AxisBank.deposit("sumit",500)
+console.log(AxisBank.listOfUsers,AxisBank.listOfEmp,AxisBank.balance);
+
+
+
 
 
 
