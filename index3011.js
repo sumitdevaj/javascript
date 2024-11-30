e887b9e9b18862aa34a05cc6b267432185a93a8d
sumitdevraj <preetam@dice-academy.com>
2024-11-30 18:01:21 +0530

    jfhjkdfejk

--- index3011.js
+++ index3011.js
@@ -13,28 +13,54 @@ function CreateBank (bankName,bankCode,bankAdress,bankBranches,helpline,numOfAtm
         let user = new CreateBankUser(userName,userId,userRole,email,phone,dob,gender,address,userAccountNumber,balance);
         this.listOfUsers.push(user)
     }
-    this.userBalance = function(){
-
+    this.userBalance = function(userId){
+        let data= this.listOfUsers.find(user=>user.userId === userId)
+        if(!data){
+            return 'account not exist'
+        }
+        return ` ${data.userName} account has this avaliable balance ${data.balance}`
     }
     this.userStatus = function(){
 
     }
-    this.userDeposit = function(){
+    this.userDeposit = function(userId,amount){
+        let data= this.listOfUsers.find(user=>user.userId === userId)
+        if(!data){
+            return 'account not exist'
+        }
+        data.balance += amount;
+        return `previous amount is ${data.balance-amount} total balance ${data.balance} and you deposited ${amount} `
+
 
     }
     this.totalBalanceInBank = function(){
-
+        let total =  0;
+         for(let user of this.listOfUsers)
+        total += user.balance
+        return `total bank balance is ${total} `
     }
-    this.userWithdrawal = function(){
-
+    this.userWithdrawal = function(userId,amount){
+        let data= this.listOfUsers.find(user=>user.userId === userId)
+        if(!data){
+            return 'account not exist'
+        }
+        if(data.balance >= amount){
+            return `remaining balance is ${data.balance - amount} and you`
+        }
+        if(data.balance < amount){
+            return 'insufficient balance'
+        }
     }
 }
 function CreateBankUser(userName,userId,userRole,email,phone,dob,gender,address,userAccountNumber,balance){
+    if(phone.length>10){
+        throw new Error("phone number must be at least 10 characters")
+    }
     this.userName=userName;
     this.userId=userId;
     this.userRole=userRole;
     this.email=email;
-    this.phone=phone;
+    this.phone=phone
     this.dob=dob;
     this.gender=gender;
     this.address=address;
@@ -50,8 +76,17 @@ function CreateBankUser(userName,userId,userRole,email,phone,dob,gender,address,
 const PritamBank = new CreateBank("PritamBank",'qwerty','tilaknagar','tn','0119043420392',0);
 
 PritamBank.addUser("Abhishek","CUSTOMER","gjeokgnedkgrd@gmail.com",54322343234,'121212','male','kdjkrnfdfjlgnfslss',100)
-PritamBank.addUser("Aditya","CUSTOMER","gjeokgnedkgrd@gmail.com",54322343234,'121212','male','kdjkrnfdfjlgnfslss',100)
+PritamBank.addUser("Aditya","CUSTOMER","gjeokgnedkgrd@gmail.com",'54322343234','121212','male','kdjkrnfdfjlgnfslss',100)
+console.log(PritamBank.userBalance(1));
+console.log(PritamBank.userDeposit(1,1000));
+console.log(PritamBank.totalBalanceInBank());
+console.log(PritamBank.userWithdrawal(1,100));
+console.log(PritamBank.userWithdrawal(1, 1200));
 
-console.log(PritamBank);
+
+
+
+
+// console.log(PritamBank);
 
 
